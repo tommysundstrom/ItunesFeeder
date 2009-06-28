a4d77a80fd01b544d7985733da5d26d6c965459b
tommysundstrom <tommy@heltenkelt.se>
2009-06-28 19:09:31 +0200

    Still template (plus some status menu). Preferences seams to work. Methods for the status items are in place, but some items that the status menu is calling on is missing, so if the menu is used it will crash. Reverted back to a more complete version of controller + preferences

---
+++ test/controller_preferences_test.rb
@@ -0,0 +1,44 @@
+require "test/unit"
+require 'rubygems'
+require 'shoulda'
+require 'assert2'
+
+class Controller_preferences_Test < Test::Unit::TestCase
+
+  # Called before every test method runs. Can be used
+  # to set up fixture information.
+  def setup
+    # Do nothing
+  end
+
+  # Called after every test method runs. Can be used to tear
+  # down fixture information.
+
+  def teardown
+    # Do nothing
+  end
+
+  context "controller_preferences.rb - " do
+    setup do
+      require 'controller_preferences'
+    end
+
+    should "Initialize" do
+      assert_nothing_raised { Preferences.new }  
+    end
+
+    context "Preferences object" do
+      setup do
+        @pref = Preferences.new
+      end
+
+      should "Have a inbox path" do
+        assert {@pref.inbox == '/Users/Tommy/Movies/iTunes-inbox'}
+      end
+
+      should "Have a processed path" do
+        assert { @pref.processed == '/Users/Tommy/Movies/iTunes-processed' }
+      end
+    end
+  end
+end
